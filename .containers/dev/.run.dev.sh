977e839615218d946df08f1bdff52690e05e214f
jbl <jbl@debian-dev-jbl.debian-dev-jbl.pokus.io>
2022-06-14 16:05:35 -0400

    wip: full dockerization for dev env

---
+++ .containers/dev/.run.dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# first load all env vars
+
+source ./.env.sh
+
+# second interpolete all env vars in ./.dev.env
+chmod +x ./.interpolate.dev.env.sh
+./.interpolate.dev.env.sh
+
+source ./.dev.env
+
+# run the build from docker-copmpose yaml [.containers/dev/docker-compose.build.yml]
+docker-compose build -f ./docker-compose.build.yml
